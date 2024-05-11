3f8ca3e32918a565cdb6afcb856d9c11d10739a4
yudatchshouga <>
2024-05-11 21:37:47 +0900

    HomeController作成

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,16 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
 public class DemoApplication {
     public static void main(String[] args) {
       SpringApplication.run(DemoApplication.class, args);
-    }
-    @GetMapping("/index")
-    public String hello() {
-      return "index";
     }
 }
